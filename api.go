cb2d0494f27eaac7b808c4bc1547f14e9b8ea5ce
Rendy Kamaluddin <147012406+rendykml@users.noreply.github.com>
2025-05-27 14:34:51 +0700

    push

--- api.go
+++ api.go
@@ -14,8 +14,8 @@ func main() {
 
     r.GET("/produk", func(c *gin.Context) {
         produk := Produk{
-            Nama:  "Teh Botol",
-            Harga: 5000,
+            Nama:  "Teh Sibo",
+            Harga: 7000,
         }
 
         c.JSON(200, produk)
